47f381b7020e5207047b131bbf26cd6f89840416
Aleksandar Dinkov <alexander.n.dinkov@gmail.com>
2021-02-21 01:20:05 +0200

    update

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const x = Xray()
 
 const source = "https://sales.bcpea.org/properties?perpage=10000&p=1";
 
-parse(readPage()).then(storeCSV)
+parse(readPage()).then((data) => storeCSV(formatData(data)))
 
 async function storePage() {
     const res = await axios.get(source);
@@ -25,11 +25,11 @@ async function parse(htmlContents) {
             size: ".header .category",
             price: ".content--price .price",
             region: ".label__group:nth-child(1) .info",
-            address: ".label__group:nth-child(2) .info",
-            court: ".label__group:nth-child(3) .info",
-            bailiff: ".label__group:nth-child(4) .info",
-            period: ".label__group:nth-child(5) .info",
-            announcedOn: ".label__group:nth-child(6) .info",
+            // address: ".label__group:nth-child(2) .info",
+            court: "col:nth-child(3) .label__group:nth-child(1) .info",
+            bailiff: "col:nth-child(3) .label__group:nth-child(2) .info",
+            period: "col:nth-child(3) .label__group:nth-child(3) .info",
+            ends: "col:nth-child(3) .label__group:nth-child(4) .info",
             href: ".col--image a@href",
         }])((err, data) => {
             if(err) reject(err);
@@ -38,9 +38,32 @@ async function parse(htmlContents) {
     })
 }
 
+function formatData(arrayOfObj) {
+    return arrayOfObj.map(obj => {
+        return {
+            ...obj,
+            publishDate: takeAfter(obj.publishDate, "Публикувано на "),
+            size: takeBefore( obj.size, " кв.м" ),
+            price: takeBefore( obj.price, " лв" ),
+        };
+    })
+}
+
+function takeAfter(str, afterString, incl = false) {
+    str = str.trim();
+    const i = str.indexOf(afterString);
+    return str.substr(i + (incl? 0 : afterString.length));
+}
+
+function takeBefore(str, beforeString, incl = false) {
+    str = str.trim();
+    const i = str.indexOf(beforeString);
+    return str.substr(0, i + (incl? 0 : beforeString.length));
+}
+
 function storeCSV(arrayOfObj) {
     const keys = Object.keys(arrayOfObj[0]);
     fs.writeFileSync("parsedSales.csv",
         keys.map(i =>`"${i}"`).join(",") + "\n" +
-        arrayOfObj.map(l => keys.map(k => `"${l[k]}"`).join(",")).join("\n"));
+        arrayOfObj.map(l => keys.map(k => `"${l[k].replace(/"/g, "'")}"`).join(",")).join("\n"));
 }

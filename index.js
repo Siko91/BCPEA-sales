de3ee4aa82006331264d0cc5c6ad1a0481e42857
Aleksandar Dinkov <alexander.n.dinkov@gmail.com>
2021-02-22 11:18:39 +0200

    uncomment

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const axios = require('axios');
 const fs = require('fs');
+
+const sqlite3 = require('sqlite3')
+const sqlite = require('sqlite')
+
 const Xray = require('x-ray')
 const x = Xray()
 
 const source = "https://sales.bcpea.org/properties?perpage=10000&p=1";
 
-parse(readPage()).then((data) => storeCSV(formatData(data)))
+(async () => {
+    await storePage();
+    const html = readPage();
+    let data = await parse(html);
+    data = formatData(data);
+    storeCSV(data);
+    await storeSQLite(data)
+})()
 
 
 async function storePage() {
@@ -37,6 +48,7 @@ async function parse(htmlContents) {
                 reject(err);
             }
             else {
+                console.log(`Parsed ${data.length} records`)
                 resolve(data);
             }
         })
@@ -45,25 +57,58 @@ async function parse(htmlContents) {
 
 function formatData(arrayOfObj) {
     return arrayOfObj.map(obj => {
-        return {
+        const result = {
+            id: takeAfter(obj.href, "properties/"),
             ...obj,
             publishDate: takeAfter(obj.publishDate, "Публикувано на "),
-            size: takeBefore( obj.size, " кв.м" ),
-            price: takeBefore( obj.price, " лв" ),
+            size: noSpace(takeBefore( obj.size, " кв.м" )),
+            price: noSpace(takeBefore( obj.price, " лв" )),
+            href: "https://sales.bcpea.org" + obj.href,
         };
+        result.pricePerMeter = (parseFloat(result.price) / parseFloat(result.size)) + "";
+        result.region = "България " + result.region;
+        result.court = "България " + result.court;
+        return result;
     })
 }
 
-function takeAfter(str, afterString, incl = false) {
+function takeAfter(str, afterString) {
     str = str.trim();
     const i = str.indexOf(afterString);
-    return str.substr(i + (incl? 0 : afterString.length));
+    return str.substr(i + afterString.length);
 }
 
-function takeBefore(str, beforeString, incl = false) {
+function takeBefore(str, beforeString) {
     str = str.trim();
     const i = str.indexOf(beforeString);
-    return str.substr(0, i + (incl? 0 : beforeString.length));
+    return str.substr(0, i);
+}
+
+function noSpace(str) { return str.replace(/ /g, ''); }
+
+async function storeSQLite(arrayOfObj) {
+    if (fs.existsSync('./parsedSales.db'))
+        fs.unlinkSync('./parsedSales.db');
+
+    const db = await sqlite.open({
+        filename: './parsedSales.db',
+        driver: sqlite3.Database
+    });
+
+    const keys = Object.keys(arrayOfObj[0]);
+    const createCommand = `CREATE TABLE sales(${
+        keys.map(i=>`"${i}" TEXT`).join(", ")
+    });`
+    await db.exec(createCommand);
+
+    for (let i = 0; i < arrayOfObj.length; i++) {
+        const command = `INSERT INTO sales (${keys.join(",")}) VALUES (${
+            keys.map(k => `"${arrayOfObj[i][k].replace(/"/g, "'")}"`).join(",")
+        })`;
+        await db.run(command)
+        if (i % 500 === 0) console.log(`Saved ${i} records in DB`)
+    }
+    console.log("Stored in DB")
 }
 
 function storeCSV(arrayOfObj) {
@@ -71,4 +116,5 @@ function storeCSV(arrayOfObj) {
     fs.writeFileSync("parsedSales.csv",
         keys.map(i =>`"${i}"`).join(",") + "\n" +
         arrayOfObj.map(l => keys.map(k => `"${l[k].replace(/"/g, "'")}"`).join(",")).join("\n"));
+    console.log("Saved CSV")
 }
